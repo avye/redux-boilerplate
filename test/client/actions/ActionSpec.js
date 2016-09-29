99de12c6307afc8db9b791c0891c5e0726c66dcb
Andrew Vye <a.vye26@gmail.com>
2016-09-29 15:18:45 -0700

    (feat) Begin the setup of the test directory

---
+++ test/client/actions/ActionSpec.js
@@ -0,0 +1,4 @@
+import { expect } from 'chai';
+
+import * as actions from '../../../client-ios/actions/index';
+import * as types from '../../../client-ios/constants/ActionTypes';
---
+++ test/server/index.js
@@ -0,0 +1,2 @@
+import expect from 'chai';
+import sinon from 'sinon';
