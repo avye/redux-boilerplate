703ed584e5883138a4298200cc80a57866c13d6a
Andrew Vye <a.vye26@gmail.com>
2016-10-09 18:17:07 -0700

    (fix) Correct a few callouts in app.js and set it to listen to the port

--- server/app.js
+++ server/app.js
@@ -1,24 +1,28 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import path from 'path';
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var path = require('path');
 
 // bring the database in
 
 var app = express();
+var port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use('/', express.static(path.resolve(__dirname, '../build')));
+app.use('/', express.static(path.resolve(__dirname, '../dist')));
 
 // Router - Bring these in from the routes
 
 // Set our defualt route
-app.use('/*', function (req, res) {
-  res.redirect('/');
-});
+// app.use('/*', function (req, res) {
+//   res.redirect('/');
+// });
+
+app.listen(port);
+console.log('Server started!');
 
 module.exports = app;
